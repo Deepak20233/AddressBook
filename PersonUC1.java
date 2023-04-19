6698a53ef699c8d1b11e94a6f05b2324267011e3
Deepak20233 <127834609+Deepak20233@users.noreply.github.com>
2023-04-19 21:54:46 +0530

    Added: contact details

---
+++ PersonUC1.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class PersonUC1 {
+
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String address;
+	private String city;
+	private int zip;
+	private String phone;
+}
